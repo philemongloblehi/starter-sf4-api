43b32ce2a2a23a366f357372ac1960ae6f244b1d
globlehi <philemongloblehi@gmail.com>
2018-07-15 07:16:53 +0000

    Implementation des fonctions CRUD pour ExpertController - cabinetController

--- src/Controller/CabinetController.php
+++ src/Controller/CabinetController.php
@@ -2,18 +2,193 @@
 
 namespace App\Controller;
 
-use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use FOS\RestBundle\Controller\FOSRestController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use Symfony\Component\HttpFoundation\Response;
+use App\Entity\Cabinet;
+use Nelmio\ApiDocBundle\Annotation\Model;
+use Swagger\Annotations as SWG;
 
-class CabinetController extends Controller
+class CabinetController extends FOSRestController
 {
     /**
-     * @Route("/cabinet", name="cabinet")
+     * @Rest\Get(
+     *      path = "/cabinets/{idCabinet}",
+     *      name = "cabinet_show",
+     *      requirements = {"idCabinet" = "\d+"}
+     * )
+     * 
+     * @SWG\Response(
+     *      response = 200,
+     *      description = "Afficher les details d'un cabinet",
+     *      @Model(type = Cabinet::class)
+     * )
+     * 
+     * @SWG\Tag(name = "Cabinet")
      */
-    public function index()
+    public function getCabinetAction(Request $request, $idCabinet)
     {
-        return $this->render('cabinet/index.html.twig', [
-            'controller_name' => 'CabinetController',
-        ]);
+        $cabinet = $this->getDoctrine()->getManager()
+                        ->getRepository(Cabinet::class)
+                        ->find($idCabinet);
+        if ($cabinet === null) {
+            return new JsonResponse(['erreur' => 400, 'message' => 'Aucun cabinet trouve'], 400);
+        }
+        return $this->view(['data' => $cabinet], Response::HTTP_OK);
     }
+
+    /**
+     * @Rest\Get(
+     *      path = "/cabinets",
+     *      name = "cabinet_list"
+     * )
+     * 
+     * @SWG\Response(
+     *      response = 200,
+     *      description = "Afficher tous les cabinets",
+     *      @Model(type = Cabinet::class)
+     * )
+     * 
+     * @SWG\Tag(name = "Cabinet")
+     */
+    public function getCabinetsAction(Request $request)
+    {
+        $cabinet = $this->getDoctrine()->getManager()
+                        ->getRepository(Cabinet::class)
+                        ->findAll();
+        if (!sizeof($cabinet) > 0) {
+            return new JsonResponse(['erreur' => 400, 'message' => 'Aucun cabinet trouve'], 400);
+        }
+        return $this->view(['data' => $cabinet], Response::HTTP_OK);
+    }
+
+    /**
+     * @Rest\Post(
+     *      path = "/cabinets",
+     *      name = "cabinet_add"
+     * )
+     * 
+     * @SWG\Response(
+     *      response = 201,
+     *      description = "Creer un nouveau cabinet",
+     *      @Model(type = Cabinet::class)
+     * )
+     * 
+     * @SWG\Parameter(
+     *      in = "body",
+     *      name = "data",
+     *      @Model(type = Cabinet::class)
+     * )
+     * 
+     * @SWG\Tag(name = "Cabinet")
+     * 
+     * @return view
+     */
+    public function postCabinetsAction(Request $request)
+    {
+        $data = json_decode($request->getContent(), true);
+        if ($data === null) {
+            return new JsonResponse(['erreur' => 400, 'message' => 'Format de donnees non valid'], 400);
+        }
+        $cabinet = new Cabinet();
+        $form = $this->createForm('App\Form\CabinetType', $cabinet);
+        $form->submit($data);
+
+        $validator = $this->get('validator');
+        $errors = $validator->validate($cabinet);
+
+        if (sizeof($errors) > 0) {
+            return $this->view(array('errors' => $errors, 'status' => Response::HTTP_BAD_REQUEST));
+        }
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($cabinet);
+        $em->flush();
+
+        return $this->view(['data' => $cabinet], Response::HTTP_OK);
+    }
+
+    /**
+     * @Rest\Put(
+     *      path = "/cabinets/{$idCabinet}",
+     *      name = "cabinet_update",
+     *      requirements = {"idCabinet" = "\d+"}
+     * )
+     * 
+     * @SWG\Response(
+     *      response = 200,
+     *      description = "Mettre à jour un cabinet",
+     *      @Model(type = Cabinet::class)
+     * )
+     * 
+     * @SWG\Parameter(
+     *      in = "body",
+     *      name = "data",
+     *      @Model(type = Cabinet::class)
+     * )
+     * 
+     * @SWG\Tag(name = "Cabinet")
+     * 
+     * @return view
+     */
+    public function putCabinetsAction(Request $request, $idCabinet)
+    {
+        $data = json_decode($request->getContent(), true);
+        if ($data === null) {
+            return new JsonResponse(['erreur' => 400, 'message' => 'Format de donnees non valid'], 400);
+        }
+
+        $cabinet = $this->getDoctrine()->getManager()
+                        ->getRepository(Cabinet::class)
+                        ->find($idCabinet);
+        if ($cabinet === null) {
+            return new JsonResponse(['erreur' => 400, 'message' => 'Cabinet non trouve'], 400);
+        }
+        $form = $this->get('validtor');
+        $errors = $validator->validate($cabinet);
+
+        if (sizeof($errors) > 0) {
+            return $this->view(array('errors' => $errors, 'status' => Response::HTTP_OK));
+        }
+    }
+
+    /**
+     * @Rest\Delete(
+     *      path = "/cabinets/{$idCabinet}",
+     *      name = "cabinet_delete"
+     * )
+     * 
+     * @SWG\Response(
+     *      response = 200,
+     *      description = "Supprimer un cabinet",
+     *      @Model(type = Cabinet::class)
+     * )
+     * 
+     * @SWG\Parameter(
+     *      in = "body",
+     *      name = "data",
+     *      @Model(type = Cabinet::class)
+     * )
+     * 
+     * @SWG\Tag(name = "Cabinet")
+     * 
+     * @return view
+     */
+    public function deleteCabinetsAction(Request $request, $idCabinet)
+    {
+        $data = json_decode($request->getContent(), true);
+        if ($data === null) {
+            return new JsonResponse(['erreur' => 400, 'message' => 'Format de donnees non valid'], 400);
+        }
+
+        $em = $this->getDoctrine()->getManager();
+        $cabinet = $em->getRepository(Cabinet::class)->find($idCabinet);
+        $em->remove($cabinet);
+        $em->flush();
+
+        return $this->view(['data' => $cabinet], Response::HTTP_OK);
+    }
+
 }
--- src/Controller/ExpertController.php
+++ src/Controller/ExpertController.php
@@ -2,18 +2,207 @@
 
 namespace App\Controller;
 
-use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use App\Entity\Expert;
+use FOS\RestBundle\Controller\FOSRestController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use Nelmio\ApiDocBundle\Annotation\Model;
+use Swagger\Annotations as SWG;
 
-class ExpertController extends Controller
+class ExpertController extends FOSRestController
 {
     /**
-     * @Route("/expert", name="expert")
+     * @Rest\Get(
+     *      path = "/experts/{idExpert}",
+     *      name = "expert_show",
+     *      requirements = {"idExpert" = "\d"}
+     * )
+     * 
+     * @SWG\Response(
+     *      response = 200,
+     *      description = "Afficher les details d'un expert",
+     *      @Model(type = Expert::class)
+     * )
+     * 
+     * @SWG\Tag(name = "Expert")
+     * 
      */
-    public function index()
+    public function getExpertAction(Request $request, $idExpert)
     {
-        return $this->render('expert/index.html.twig', [
-            'controller_name' => 'ExpertController',
-        ]);
+        $expert = $this->getDoctrine()->getManager()
+                       ->getRepository(Expert::class)
+                       ->find($idExpert);
+        if ($expert === null) {
+            return new JsonResponse(['erreur' => 400, 'message' => 'Aucun expert trouve'], 400);
+        }
+        return $this->view(['data' => $expert], Response::HTTP_OK);
     }
+
+    /**
+     * @Rest\Get(
+     *      path = "/experts",
+     *      name = "expert_list"
+     * )
+     * 
+     * @SWG\Response(
+     *      response = 200,
+     *      description = "Afficher tous les experts",
+     *      @Model(type = Expert::class)
+     * )
+     * 
+     * @SWG\Tag(name = "Expert")
+     */
+    public function getExpertsAction(Request $request)
+    {
+        $expert = $this->getDoctrine()->getManager()
+                       ->getRepository(Expert::class)
+                       ->findAll();
+        if ($expert === null) {
+            return new JsonResponse(['erreur' => 400, 'message' => 'Aucun expert dans la base de donnes'], 400);
+        }
+        return $this->view(['data' => $expert], Response::HTTP_OK);
+    }
+
+    /**
+     * @Rest\Post(
+     *      path = "/experts",
+     *      name = "expert_add"
+     * )
+     * 
+     * @SWG\Response(
+     *      response = 201,
+     *      description = "Creer un expert",
+     *      @Model(type = Expert::class)
+     * )
+     * 
+     * @SWG\Parameter(
+     *      in = "body",
+     *      name = "data",
+     *      @Model(type = Expert::class)
+     * )
+     * 
+     * @SWG\Tag(name = "Expert")
+     * 
+     * @return View
+     */
+    public function postExpertsAction(Request $request)
+    {
+        $data = json_decode($request->getContent(), true);
+        if ($data === null) {
+            return new JsonResponse(['erreur' => 400, 'message' => 'Format de donnees non valid'], 400);
+        }
+        $expert = new Expert();
+        $form = $this->createForm('App\Form\ExpertType', $expert);
+        $form->submit($data);
+
+        $validator = $this->get('validator');
+        $errors = $validator->validate($expert);
+
+        if (sizeof($errors) > 0) {
+            return $this->view(array('errors' => $errors, 'status' => Response::HTTP_BAD_REQUEST));
+        }
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($expert);
+        $em->flush();
+
+        return $this->view(['data' => $expert], Response::HTTP_OK);
+    }
+
+    /**
+     * @Rest\Put(
+     *      path = "/experts/{idExpert}",
+     *      name = "expert_update",
+     *      requirements = {"idExpert" = "\d+"}
+     * )
+     * 
+     * @SWG\Response(
+     *      response = 200,
+     *      description = "Mettre à jour un expert",
+     *      @Model(type = Expert::class)
+     * )
+     * 
+     * @SWG\Parameter(
+     *      in = "body",
+     *      name = "data",
+     *      @Model(type = Expert::class)
+     * )
+     * 
+     * @SWG\Tag(name = "Expert")
+     * 
+     * @return view
+     */
+    public function putExpertsAction(Request $request, $idExpert)
+    {
+        $data = json_decode($request->getContent(), true);
+        if ($data === null) {
+            return new JsonResponse(['erreur' =>  400, 'message' => 'Format de donnees non valid'], 400);
+        }
+        $expert = $this->getDoctrine()->getManager()
+                       ->getRepository(Expert::class)
+                       ->find($idExpert);
+        if ($expert === null) {
+            return new JsonResponse(['erreur' => 400, 'message' => 'Expert non trouve'], 400);
+        }
+        $form = $this->createForm('App\Form\ExpertType', $expert);
+        $form->submit($data);
+
+        $validator = $this->get('validator');
+        $errors = $validator->validate($expert);
+
+        if (sizeof($errors) > 0) {
+            return $this->view(array('errors' => $errors, 'status' => Response::HTTP_BAD_REQUEST));
+        }
+
+        $em = $this->getDoctrine()->getManager();
+        $em->flush();
+
+        return $this->view(['data' => $expert], Response::HTTP_OK);
+    }
+
+    /**
+     * Rest\Delete(
+     *      path = "/experts/{$idExpert}",
+     *      name = "expert_delete"
+     * )
+     * 
+     * @SWG\Response(
+     *      response = 200,
+     *      description = "Supprimer un expert",
+     *      @Model(type = Expert::class)
+     * )
+     * 
+     * @SWG\Parameter(
+     *      in = "body",
+     *      name = "data",
+     *      @Model(type = Expert::class)
+     * )
+     * 
+     * @SWG\Tag(name = "Expert")
+     * 
+     * @return view
+     */
+    public function deleteExpertsAction(Request $request, $idExpert)
+    {
+        $data = json_decode($request->getContent(), true);
+        if ($data === null) {
+            return new JsonResponse(['erreur' => 400, 'message' => 'Format de donnees non valid'], 400);
+        }
+        $expert = $this->getDoctrine()->getManager()
+                       ->getRepository(Expert::class)
+                       ->find($idExpert);
+        if ($expert === null) {
+            return new JsonResponse(['erreur' => 400, 'message' => 'Expert non trouve'], 400);
+        }
+
+        $em = $this->getDoctrine()->getManager();
+        $expert = $em->getRepository(Expert::class)->find($idExpert);
+        $em->remove($expert);
+        $em->flush();
+
+        return $this->view(['data' => $expert], Response::HTTP_OK);
+    }
+
 }
